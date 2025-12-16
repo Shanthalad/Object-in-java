32649e2e213526a5751fe54891b549ea6836d872
Lenovo <Lenovo@DESKTOP-PNAR7AQ>
2025-12-16 21:04:15 +0530

    Uses of Object in java

---
+++ src/ObejctExample.java
@@ -0,0 +1,23 @@
+
+public class ObejctExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee emp=new Employee();
+		System.out.println("Employee name:"+emp.name);
+		System.out.println("Employee age:"+emp.age);
+		System.out.println("Employee course:"+emp.course);
+		System.out.println("Employee id:"+emp.id);
+		
+
+	}
+
+}
+
+class Employee{
+	String name="Radhika";
+	int age=25;
+	String course="Java";
+	int id=300284;
+	
+}
